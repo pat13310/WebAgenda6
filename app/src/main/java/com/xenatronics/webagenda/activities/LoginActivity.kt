a0969fa3ea8bda55e4edacb2763552f4bece025d
pat13310 <92207286+pat13310@users.noreply.github.com>
2022-03-05 10:55:56 +0100

    Activités : Ajout des coquilles vides LoginActivity et SplashActivity

---
+++ app/src/main/java/com/xenatronics/webagenda/activities/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.xenatronics.webagenda.activities
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginActivity(navigation:Unit){
+    Column() {
+
+    }
+}
---
+++ app/src/main/java/com/xenatronics/webagenda/activities/SplashActivity.kt
@@ -0,0 +1,8 @@
+package com.xenatronics.webagenda.activities
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashActivity(navController:Unit){
+
+}
