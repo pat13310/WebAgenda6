1bf66e3698bd5ed3c5771d3d75a4abae3b374f51
pat13310 <92207286+pat13310@users.noreply.github.com>
2022-03-06 23:34:42 +0100

    Implémentation du squelette de LoginActivity.kt

--- app/src/main/java/com/xenatronics/webagenda/activities/LoginActivity.kt
+++ app/src/main/java/com/xenatronics/webagenda/activities/LoginActivity.kt
@@ -1,12 +1,45 @@
 package com.xenatronics.webagenda.activities
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.xenatronics.webagenda.Action
+import com.xenatronics.webagenda.components.NewTaskBar
+import com.xenatronics.webagenda.navigation.Screen
 
 @Composable
-fun LoginActivity(navController:NavController){
-    Column() {
-
+fun LoginActivity(navController: NavController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Scaffold(
+            topBar = {
+                NewTaskBar(
+                    "Connexion",
+                    NavigateToListScreen = { action ->
+                        if (action == Action.ADD) {
+                            navController.navigate(Screen.AddScreen.route)
+                        }
+                    },
+                    noBack = true
+                )
+            },
+            content = {
+                LoginContent(modifier = Modifier, navController)
+            }
+        )
     }
+}
+
+@Composable
+fun LoginContent(
+    modifier: Modifier,
+    navController: NavController
+) {
+
 }
