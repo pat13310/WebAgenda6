bfb0741a92cd35cb113091bca104664a27b72c10
pat13310 <92207286+pat13310@users.noreply.github.com>
2022-03-07 20:10:58 +0100

    Ajout des fonctions de mise à jour et suppression

--- app/src/main/java/com/xenatronics/webagenda/repository/RepositoryRdv.kt
+++ app/src/main/java/com/xenatronics/webagenda/repository/RepositoryRdv.kt
@@ -1,24 +1,23 @@
 package com.xenatronics.webagenda.repository
 
-import com.xenatronics.webagenda.data.CardResponse
+import com.xenatronics.webagenda.data.ResponseRDV
 import com.xenatronics.webagenda.data.PostRequest
 import com.xenatronics.webagenda.data.Rdv
 import com.xenatronics.webagenda.network.KtorClient
-import com.xenatronics.webagenda.util.Constants.ADD_TASK
-import com.xenatronics.webagenda.util.Constants.CLEAR_TASKS
-import com.xenatronics.webagenda.util.Constants.DEL_TASK
-import com.xenatronics.webagenda.util.Constants.GET_TASKS
+import com.xenatronics.webagenda.util.Constants.ADD_RDV
+import com.xenatronics.webagenda.util.Constants.CLEAR_RDV
+import com.xenatronics.webagenda.util.Constants.DEL_RDV
+import com.xenatronics.webagenda.util.Constants.GET_ALL_RDV
+import com.xenatronics.webagenda.util.Constants.UPDATE_RDV
 import io.ktor.client.features.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.http.cio.*
 
 object RepositoryRdv {
 
-
     suspend fun getListe():List<Rdv>{
         return try {
-            KtorClient.httpClient.get{url(GET_TASKS)}
+            KtorClient.httpClient.get{url(GET_ALL_RDV)}
         }
         catch (e: RedirectResponseException){
             // 3xx --
@@ -41,22 +40,22 @@ object RepositoryRdv {
             emptyList()
         }
     }
-    suspend fun clearListe():CardResponse{
+    suspend fun clearListe():ResponseRDV{
         return KtorClient.httpClient.use {
-            it.get(CLEAR_TASKS)
+            it.get(CLEAR_RDV)
         }
     }
 
-    suspend fun clearRdv(id: Int): CardResponse {
+    suspend fun clearRdv(id: Int): ResponseRDV {
         return KtorClient.httpClient.use {
-            it.get("$DEL_TASK/$id")
+            it.get("$DEL_RDV/$id")
         }
     }
 
-    suspend fun addRdv(rdv: PostRequest):List<CardResponse>{
+    suspend fun addRdv(rdv: PostRequest):List<ResponseRDV>{
         return try {
             KtorClient.httpClient.post{
-                url(ADD_TASK)
+                url(ADD_RDV)
                 contentType(ContentType.Application.Json)
                 body=rdv
             }
@@ -83,4 +82,35 @@ object RepositoryRdv {
         }
 
         }
+
+    suspend fun updateRdv(rdv: PostRequest):List<ResponseRDV>{
+        return try {
+            KtorClient.httpClient.put{
+                url(UPDATE_RDV)
+                contentType(ContentType.Application.Json)
+                body=rdv
+            }
+        }
+        catch (e: RedirectResponseException){
+            // 3xx --
+            println(e.response.status.description)
+            emptyList()
+        }
+        catch (e:ClientRequestException){
+            // 4xx --
+            println(e.response.status.description)
+            emptyList()
+        }
+        catch (e:ServerResponseException){
+            // 5xx --
+            println(e.response.status.description)
+            emptyList()
+        }
+        catch (e:Exception){
+            // xxx --
+            println(e.message)
+            emptyList()
+        }
+
+    }
     }
