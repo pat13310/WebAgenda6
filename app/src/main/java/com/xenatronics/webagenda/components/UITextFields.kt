6615a550dd136432c479abfed9176c4ed2d8613b
pat13310 <92207286+pat13310@users.noreply.github.com>
2022-03-07 20:10:16 +0100

    Amélioration visuelle des UITextStandard

--- app/src/main/java/com/xenatronics/webagenda/components/UITextFields.kt
+++ app/src/main/java/com/xenatronics/webagenda/components/UITextFields.kt
@@ -5,22 +5,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.filled.VpnKey
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.unit.dp
 import com.xenatronics.webagenda.activities.TOP_SPACE
 import com.xenatronics.webagenda.util.Constants
-import com.xenatronics.webagenda.util.Constants.RADIUS_MEDIUM
 import com.xenatronics.webagenda.util.Constants.RADIUS_SMALL
 
 
@@ -33,6 +31,12 @@ fun UITextStandard(
     keyboardType: KeyboardType = KeyboardType.Text
 ) {
     OutlinedTextField(
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            textColor = MaterialTheme.colors.primary,
+            leadingIconColor = MaterialTheme.colors.primary,
+            trailingIconColor = MaterialTheme.colors.primary,
+            unfocusedBorderColor = MaterialTheme.colors.primary
+        ),
         value = value,
         leadingIcon = {
             Icon(
@@ -63,6 +67,12 @@ fun UITextPassword(
     var visibility by remember { mutableStateOf(false) }
 
     OutlinedTextField(
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            textColor = MaterialTheme.colors.primary,
+            leadingIconColor = MaterialTheme.colors.primary,
+            trailingIconColor = MaterialTheme.colors.primary,
+            unfocusedBorderColor = MaterialTheme.colors.primary
+        ),
         value = value,
         leadingIcon = {
             Icon(
@@ -86,7 +96,7 @@ fun UITextPassword(
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = TOP_SPACE)
-        .height(Constants.HEIGHT_COMPONENT),
+            .height(Constants.HEIGHT_COMPONENT),
         visualTransformation = if (visibility) VisualTransformation.None else PasswordVisualTransformation()
     )
 }
