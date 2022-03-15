b9c313a479f9c6245c7c3dc059e419a9f11a0bb0
pat13310 <92207286+pat13310@users.noreply.github.com>
2022-03-15 23:11:34 +0100

    Fix mineur

---
+++ app/src/main/java/com/xenatronics/webagenda/components/CardArrow.kt
@@ -0,0 +1,37 @@
+package com.xenatronics.webagenda.components
+
+import android.annotation.SuppressLint
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import com.xenatronics.webagenda.R
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Composable
+fun CardArrow(
+    modifier: Modifier,
+    degrees: Float,
+    onClick: () -> Unit
+) {
+    IconButton(
+        onClick = onClick,
+        content = {
+            Icon(
+                painter = painterResource(id = R.drawable.drop_up),
+                contentDescription = "Expandable Arrow",
+                modifier = Modifier.rotate(degrees),
+            )
+        }
+    )
+}
+
+@SuppressLint("SimpleDateFormat")
+fun convertTime(timestamp: Long): String {
+    val simpleDateFormat =
+        SimpleDateFormat("'Le' dd MMMM yyyy 'à' HH:mm", Locale.FRANCE)
+    return simpleDateFormat.format(timestamp * 1000L)
+}
